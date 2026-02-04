0c3b796965d33bd2b41acb831e27f4dd8c16d444
Andrew Gossage <gosssagefront@gmail.com>
2026-02-03 20:54:16 -0700

    added stdin

---
+++ zig/input.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+
+pub fn main(init: std.process.Init) !void {
+    const io = init.io;
+    var stdin_buffer: [1024]u8 = undefined;
+    var stdin_reader = std.Io.File.stdin().reader(io, &stdin_buffer);
+    const stdin = &stdin_reader.interface;
+
+    const name = try stdin.takeDelimiterExclusive('\n');
+    _ = name;
+}
