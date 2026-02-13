e9160af91c63e4cdc39f0d7b643f4d070b66411e
Andrew Gossage <gosssagefront@gmail.com>
2026-02-12 17:49:24 -0700

    small tweaks

--- zig/input-03.zig
+++ zig/input-03.zig
@@ -1,13 +1,9 @@
-// zig run hello-02.zig -- hello world
+// zig run input-03.zig -- hello world
 const std = @import("std");
 const Io = std.Io;
 
 pub fn main(init: std.process.Init) !void {
-    // Prints to stderr, unbuffered, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    // This is appropriate for anything that lives as long as the process.
     const arena: std.mem.Allocator = init.arena.allocator();
-    // Accessing command line arguments:
     const args = try init.minimal.args.toSlice(arena);
     for (args, 0..args.len) |arg, i| {
         if (i > 0) {
